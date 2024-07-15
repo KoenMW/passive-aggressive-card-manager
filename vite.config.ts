21cfbfcc085ce4616ff0472f36cfdc51e6d6744d
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-07-15 15:29:11 +0200

    chore: Update Node.js version to "20" in GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/passive-aggressive-card-manager",
   resolve: {
     alias: {
       "@stores": path.resolve(__dirname, "./src/stores"),

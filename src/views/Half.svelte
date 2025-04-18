eef8e3df5938f97eee93093345f8cd7457d85e7e
KoenMW <89910070+KoenMW@users.noreply.github.com>
2025-04-18 21:45:12 +0200

    fix: half view filters

--- src/views/Half.svelte
+++ src/views/Half.svelte
@@ -2,13 +2,17 @@
   import Button from "@components/Button.svelte";
   import CardFilter from "@components/CardFilter.svelte";
   import Cardhalf from "@components/cardhalf.svelte";
-  import { type cardNames } from "@core/types";
+  import { type cardHalf, type cardNames } from "@core/types";
   import { cards } from "@stores/card";
   import { route } from "@stores/router";
 
   let currentFilter: cardNames = "None";
   let stringFilter: string = "";
   let filterPassive: boolean = false;
+
+  const checkPassiveFilter = (half: cardHalf, passiveFilter: boolean) => {
+    return !passiveFilter || half.cardType.passive;
+  };
 
   $: halves = $cards
     .map((card) => {
@@ -18,13 +22,22 @@
     })
     .flat()
     .filter((half) => {
-      return (
-        ((currentFilter === "None" || half.cardType.type === currentFilter) &&
+      console.log(
+        half.text
+          .toLocaleLowerCase()
+          .includes(stringFilter.toLocaleLowerCase()),
+        (currentFilter === "None" || half.cardType.type === currentFilter) &&
           half.text
             .toLocaleLowerCase()
             .includes(stringFilter.toLocaleLowerCase()) &&
-          !filterPassive) ||
-        half.cardType.passive
+          checkPassiveFilter(half, filterPassive)
+      );
+      return (
+        (currentFilter === "None" || half.cardType.type === currentFilter) &&
+        half.text
+          .toLocaleLowerCase()
+          .includes(stringFilter.toLocaleLowerCase()) &&
+        checkPassiveFilter(half, filterPassive)
       );
     });
 </script>
